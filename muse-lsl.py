ad802d6c801ca1e58304656f493fb3b3b5f42e18
Michael Flaks <misha.flaks@gmail.com>
2017-12-06 00:33:25 -0700

    Rewrite muse-lsl with argparse
    Switch from obsolete optparse to argparse, change coding according to
    PEP

--- muse-lsl.py
+++ muse-lsl.py
@@ -1,57 +1,114 @@
-from muse import Muse
-from time import sleep
-from pylsl import StreamInfo, StreamOutlet, local_clock
-from optparse import OptionParser
+#!/usr/bin/env python
+'''Script to connect Muse 2016 by Bluetooth and stream data from it through
+LSL. This will create an outlet for EEG in LSL and start pushing samples
+through it.'''
 
-parser = OptionParser()
-parser.add_option("-a", "--address",
-                  dest="address", type='string', default=None,
-                  help="device mac adress.")
-parser.add_option("-n", "--name",
-                  dest="name", type='string', default=None,
-                  help="name of the device.")
-parser.add_option("-b", "--backend",
-                  dest="backend", type='string', default="auto",
-                  help="pygatt backend to use. can be auto, gatt or bgapi")
-parser.add_option("-i", "--interface",
-                  dest="interface", type='string', default=None,
-                  help="The interface to use, 'hci0' for gatt or a com port for bgapi")
+import argparse
+import logging
+import time
 
-(options, args) = parser.parse_args()
+# from pylsl import StreamInfo, StreamOutlet, local_clock
+import pylsl
 
-info = info = StreamInfo('Muse', 'EEG', 5, 256, 'float32',
-                         'Muse%s' % options.address)
-
-info.desc().append_child_value("manufacturer", "Muse")
-channels = info.desc().append_child("channels")
-
-for c in ['TP9', 'AF7', 'AF8', 'TP10', 'Right AUX']:
-    channels.append_child("channel") \
-        .append_child_value("label", c) \
-        .append_child_value("unit", "microvolts") \
-        .append_child_value("type", "EEG")
-outlet = StreamOutlet(info, 12, 360)
+# from muse import Muse
+import muse
 
 
-def process(data, timestamps):
-    for ii in range(12):
-        outlet.push_sample(data[:, ii], timestamps[ii])
+def main(args):
+    logging.basicConfig(level=args.loglevel or logging.INFO)
 
-muse = Muse(address=options.address, callback=process,
-            backend=options.backend, time_func=local_clock,
-            interface=options.interface, name=options.name)
+    info = pylsl.StreamInfo(
+        name='Muse',
+        type='EEG',
+        channel_count=5,
+        nominal_srate=256,
+        channel_format='float32',
+        source_id='Muse%s' % args.address)
 
-muse.connect()
-print('Connected')
-muse.start()
-print('Streaming')
+    info.desc().append_child_value("manufacturer", "Muse")
+    channels = info.desc().append_child("channels")
 
-while 1:
-    try:
-        sleep(1)
-    except:
-        break
+    for c in ['TP9', 'AF7', 'AF8', 'TP10', 'Right AUX']:
+        channels.append_child("channel") \
+                .append_child_value("label", c) \
+                .append_child_value("unit", "microvolts") \
+                .append_child_value("type", "EEG")
 
-muse.stop()
-muse.disconnect()
-print('Disonnected')
+        outlet = pylsl.StreamOutlet(info, 12, 360)
+
+    def process(data, timestamps):
+        for ii in range(12):
+            outlet.push_sample(data[:, ii], timestamps[ii])
+
+    m = muse.Muse(
+        address=args.address,
+        callback=process,
+        backend=args.backend,
+        time_func=pylsl.local_clock,
+        interface=args.interface,
+        name=args.name)
+
+    return m
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=__doc__)
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        '-v',
+        '--verbose',
+        help='verbose (debug) logging',
+        action='store_const',
+        const=logging.DEBUG,
+        dest='loglevel')
+    parser.add_argument(
+        "-a",
+        "--address",
+        help="device MAC address",
+        action='store',
+        dest="address",
+        default=None,
+        type=str)
+    parser.add_argument(
+        "-n",
+        "--name",
+        help="name of the device",
+        action='store',
+        dest="name",
+        default=None,
+        type=str)
+    parser.add_argument(
+        "-b",
+        "--backend",
+        help="pygatt backend to use, can be auto, gatt or bgapi",
+        action='store',
+        dest="backend",
+        default="auto",
+        choices=['auto', 'gatt', 'bgapi'],
+        type=str)
+    parser.add_argument(
+        "-i",
+        "--interface",
+        help="interface to use, 'hci0' for 'gatt' or a com port for bgapi",
+        action='store',
+        dest="interface",
+        default=None,
+        type=str)
+
+    args = parser.parse_args()
+
+    muse = main(args)
+    muse.connect()
+    print('Muse%s connected.' % args.address)
+    muse.start()
+    print('Muse streaming started.')
+
+    while True:
+        try:
+            time.sleep(1)
+        except:
+            break
+        finally:
+            muse.stop()
+            muse.disconnect()
+            logging.info('Muse disconnected.')
